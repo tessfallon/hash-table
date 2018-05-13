e84114feefe7e98749bcf425e995514acc8e6765
Tess Fallon <tesssfallon23@gmail.com>
2018-05-13 11:52:51 -0500

    changes to document

--- linear_probing.py
+++ linear_probing.py
@@ -4,3 +4,30 @@ class LinearProbing:
         self.size = 19
         self.n = 7
         self.table = [None]*self.size
+
+    def __nextPrime(self):
+        pass
+
+    def hashFunction(self):
+        pass
+
+    def __shouldResize(self):
+        pass
+
+    def insert(self, value):
+        pass
+
+    def __resize(self):
+        pass
+    
+    def checkValue(self):
+        pass
+
+    def findPosition(self):
+        pass
+
+    def __str__(self):
+        pass
+
+if __name__ == '__main__':
+    pass
