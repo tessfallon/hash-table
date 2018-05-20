b0786c68f50ac1881afda5e1640f9688c3bde541
Tess Fallon <tessfallon23@gmail.com>
2018-05-20 17:00:01 -0500

    changes to linear-probing

--- linear_probing.py
+++ linear_probing.py
@@ -9,17 +9,31 @@ class LinearProbing:
         self.size = 2 ** self.n - 1
         self.n += 1
 
-    def hashFunction(self):
-        pass
+    def hashFunction(self, value):
+        return(15*value + value**2 +5)%self.size
 
     def __shouldResize(self):
         return True if self.fullness / self.size >= self.percent_full else False
 
     def insert(self, value):
-        pass
+        position = self.hashFunction(value)
+        if self.table[position] is None:
+            self.table[position] = value
+        else:
+            while self.table[position] is not None:
+                if position > self.size:
+                    position = 0
+                else:
+                    position += 1
+            self.table[position] = value
+        if (self._shouldResize()):
+            self._resize()
 
     def __resize(self):
-        pass
+        self.size = self._nextPrime()
+        copy = self.table
+        for number in copy:
+            self.insert(number)
 
     def checkValue(self):
         pass
@@ -40,4 +54,9 @@ class LinearProbing:
         return hashTableString
 
 if __name__ == '__main__':
-    pass
+    self._nextPrime()
+    copy = self.list
+    self.list = [[] for x in range(self.size)]
+    for bucket in copy:
+        for value in bucket:
+            self.insert(value)
