dc5968b13609662aefa86426145c1b018c3bfca2
Avery Lieu <avelieu@gmail.com>
2018-05-20 16:23:34 -0500

    Test branch (#2)
    started linear probing

--- linear_probing.py
+++ linear_probing.py
@@ -6,20 +6,21 @@ class LinearProbing:
         self.table = [None]*self.size
 
     def __nextPrime(self):
-        pass
+        self.size = 2 ** self.n - 1
+        self.n += 1
 
     def hashFunction(self):
         pass
 
     def __shouldResize(self):
-        pass
+        return True if self.fullness / self.size >= self.percent_full else False
 
     def insert(self, value):
         pass
 
     def __resize(self):
         pass
-    
+
     def checkValue(self):
         pass
 
@@ -27,7 +28,16 @@ class LinearProbing:
         pass
 
     def __str__(self):
-        pass
+        hashTableString = '| '
+        for bucket in self.list:
+            if len(bucket) == 0:
+                hashTableString += '[NONE] '
+            else:
+                hashTableString += str(bucket) + ' '
+        hashTableString = hashTableString[:-1]
+        hashTableString += ' |'
+
+        return hashTableString
 
 if __name__ == '__main__':
     pass
